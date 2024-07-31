0f0a814eb515b1d56d9db5c74cd04fd0769eaefc
IrminDev <ihernandezj1902@alumno.ipn.mx>
2024-07-30 23:22:24 -0600

    ex 2-6

--- material/log-output-endpoint/log-server/index.js
+++ material/log-output-endpoint/log-server/index.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 const logFilePath = 'files/data.txt';
+const informationPath = '/etc/config/information.txt';
 const path = require('path');
 const axios = require('axios');
 const urlPingPong = process.env.PING_PONG_URL || 'http://localhost:3001';
+const envMessage = process.env.MESSAGE;
 
 // Create a directory for the logs if it doesn't exist
 fs.mkdirSync(path.dirname(logFilePath), { recursive: true });
@@ -30,12 +32,24 @@ app.get('/', (req, res) => {
       return;
     }
 
+    // Read the information file
+    let information = '';
+    try {
+      information = fs.readFileSync(informationPath, 'utf8');
+      console.log('Information file read successfully');
+      console.log(information);
+    } catch (err) {
+      console.log('Error reading information file');
+      console.log(err);
+    }
+
     console.log('Getting counter from ping-pong service');
     // Get the counter from ping-pong service
     const response = await axios.get(`${urlPingPong}/pingpong`);
     const counter = response.data;
 
-    res.send(`<pre>${data}ping pong ${counter}</pre>`);
+    // Send the response
+    res.send(`${data}\n ${information}\n Ping Pongs: ${counter}\n Message: ${envMessage}`);
   });
 });
 
