b2d0714b3066c19dd0690d3d8949729af9cb2c23
IrminDev <ihernandezj1902@alumno.ipn.mx>
2024-08-02 18:07:49 -0600

    ex 3-3

--- material/todo-backend/src/test/java/com/github/irmindev/todo_backend/TodoBackendApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.github.irmindev.todo_backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TodoBackendApplicationTests {
-
-}
